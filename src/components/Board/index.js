9618b9032e4bc5af4489402f6a6a290dadea0db6
Rohit Singh <rohitssingh1610@gmail.com>
2023-12-24 14:12:38 +0530

    redux strore added

---
+++ src/components/Board/index.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+import { useSelector,useDispatch } from 'react-redux'
+
+const Board = () => {
+  const canvasRef = useRef(null)
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+  const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
+
+  useEffect(() => {
+    if (!canvasRef.current) return
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d')
+
+    //when mounting
+    canvas.width = window.width
+    canvas.height = window.innerHeight
+  }, [])
+
+  return (<canvas ref={canvasRef}></canvas>);
+}
+
+export default Board;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 import styles from './index.module.css'
+import cx from 'classnames'
+import { useDispatch, useSelector } from 'react-redux'
+import { MENU_ITEMS } from '@/constants'
+import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
 
 const Menu = () => {
+  const dispatch = useDispatch();
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+  //console.log(activeMenuItem === MENU_ITEMS.PENCIL)
+  const handleMenuClick = (itemName) => {
+    dispatch(menuItemClick(itemName))
+  }
+
   return (
     <div className={styles.menuContainers}>
-      <div className={styles.iconWrapper}>
-        <FontAwesomeIcon icon={faPencil} className={styles.icon}/>
+      <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.PENCIL})} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
+        <FontAwesomeIcon icon={faPencil} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
-        <FontAwesomeIcon icon={faEraser} className={styles.icon}/>
+      <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.ERASER})} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
+        <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
-        <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
+      <div className={styles.iconWrapper} >
+        <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
       </div>
       <div className={styles.iconWrapper}>
         <FontAwesomeIcon icon={faRotateRight} className={styles.icon}/>
