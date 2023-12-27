ad3c756c2963381592bceef70004c2d12d409f38
Rohit Singh <rohitssingh1610@gmail.com>
2023-12-27 22:24:38 +0530

    Download feature given

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -9,9 +9,13 @@ import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
 const Menu = () => {
   const dispatch = useDispatch();
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
-  //console.log(activeMenuItem === MENU_ITEMS.PENCIL)
+  
   const handleMenuClick = (itemName) => {
     dispatch(menuItemClick(itemName))
+  }
+
+  const handleActionItemClick = (itemName) => {
+    dispatch(actionItemClick(itemName))
   }
 
   return (
@@ -22,13 +26,13 @@ const Menu = () => {
       <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.ERASER})} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper} >
+      <div className={styles.iconWrapper} onClick={() => handleActionItemClick(MENU_ITEMS.UNDO)}>
         <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div className={styles.iconWrapper} onClick={() => handleActionItemClick(MENU_ITEMS.REDO)}>
         <FontAwesomeIcon icon={faRotateRight} className={styles.icon}/>
       </div>
-      <div className={styles.iconWrapper}>
+      <div className={styles.iconWrapper} onClick={() => handleActionItemClick(MENU_ITEMS.DOWNLOAD)}>
         <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon}/>
       </div>
     </div>
